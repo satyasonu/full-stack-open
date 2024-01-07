dca1b2b5429947e60c402332669c63ec16984a19
satyasonu <sbssunu1@gmail.com>
2024-01-07 13:46:20 +0530

    1.3: course information step3 completed

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import Part from "./Part";
 
 const Content = ({ content }) => {
-  const { part1, exercises1, part2, exercises2, part3, exercises3 } = content;
+  const { part1, part2, part3 } = content;
   return (
     <>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      <Part part={part1.name} exercises={part1.exercises} />
+      <Part part={part2.name} exercises={part2.exercises} />
+      <Part part={part3.name} exercises={part3.exercises} />
     </>
   );
 };
--- part1/courseinfo/src/Total.jsx
+++ part1/courseinfo/src/Total.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const Total = ({exercises}) => {
-  const {exercises1, exercises2, exercises3} = exercises;
+  const {part1, part2, part3} = exercises;
   return (
-    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
   )
 }
 
