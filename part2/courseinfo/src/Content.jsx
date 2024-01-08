245a871ab5627b7da2836a55b354bd0cc6d43a31
satyasonu <sbssunu1@gmail.com>
2024-01-08 18:18:23 +0530

    2.1: Course information step6 completed

--- part2/courseinfo/src/Content.jsx
+++ part2/courseinfo/src/Content.jsx
@@ -4,8 +4,8 @@ import Part from "./Part";
 const Content = ({ content }) => {
   return (
     <>
-      {content.map((oneContent, index) => {
-        return (<Part key={index} part={oneContent.name} exercises={oneContent.exercises} />)
+      {content.map((oneContent) => {
+        return (<Part key={oneContent.id} part={oneContent.name} exercises={oneContent.exercises} />)
       })}
     </>
   );
